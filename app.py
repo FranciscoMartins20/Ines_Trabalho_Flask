de4b6d86f59bc9468e80e20ae4f5188225c42089
Francisco Martins <jfmartins200@gmail.com>
2024-04-02 22:45:17 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, template_folder='./frontend/templates', static_folder='./frontend/static')
+app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'  # Defina uma chave secreta para proteger suas sessões
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://@DESKTOP-GHNLUF7\\SQLEXPRESS/clinica?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    __tablename__ = 'User'
+    email = db.Column(db.String(120), primary_key=True)
+    nome = db.Column(db.String(20), nullable=False) 
+    password = db.Column(db.String(128), nullable=False) 
+    role = db.Column(db.String(50), nullable=False)
+
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+# Formulário de Registro
+class RegistrationForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()]) 
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+# Formulário de Login
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+# Rota da Página Inicial
+@app.route('/')
+def home():
+    return 'Página inicial'
+
+# Rota de Registro
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(nome=form.nome.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+# Rota de Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
